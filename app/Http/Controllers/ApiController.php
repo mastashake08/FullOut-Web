299c88a242ee1bc206f6dba31453d30c34bd0205
Jyrone Parker <jyrone.parker@gmail.com>
2017-03-24 13:53:34 -0400

    url

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -22,7 +22,7 @@ class ApiController extends Controller
 
       $http = new \GuzzleHttp\Client;
 
-      $response = $http->post('http://192.241.140.151/oauth/token', [
+      $response = $http->post(url('/oauth/token'), [
           'form_params' => [
               'grant_type' => 'password',
               'client_id' => $this->client_id,
@@ -40,7 +40,7 @@ class ApiController extends Controller
 
       $http = new \GuzzleHttp\Client;
 
-      $response = $http->post('http://192.241.140.151/oauth/token', [
+      $response = $http->post(url('/oauth/token'), [
           'form_params' => [
               'grant_type' => 'password',
               'client_id' => $this->client_id,
