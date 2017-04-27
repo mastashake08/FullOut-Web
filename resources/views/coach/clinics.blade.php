b4c17503a6bac41df362d19e9caaf080982f5ce5
Jyrone Parker <jyrone.parker@gmail.com>
2017-04-27 12:39:48 -0400

    view seperation

---
+++ resources/views/coach/clinics.blade.php
@@ -0,0 +1,88 @@
+<div class="row">
+    <div class="col-md-8 col-md-offset-2">
+        <div class="panel panel-default">
+            <div class="panel-heading">Add Clinic</div>
+
+            <div class="panel-body">
+              <form class="form" role="form" action="{{url('/coach/clinics')}}" method="post">
+                {!!csrf_field()!!}
+                <input class="form-control"  name="name" placeholder="Clinic Name" value="{{old('name')}}"/>
+                @if ($errors->has('name'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('name') }}</strong>
+                    </span>
+                @endif
+                <input class="form-control"  name="coach_name" placeholder="Coach Name" value="{{old('coach_name')}}"/>
+                @if ($errors->has('coach_name'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('coach_name') }}</strong>
+                    </span>
+                @endif
+                <input class="form-control date" type="date"  name="start_datetime" placeholder="Start Date" value="{{old('start_datetime')}}"/>
+                @if ($errors->has('start_datetime'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('start_datetime') }}</strong>
+                    </span>
+                @endif
+                <input class="form-control date" type="date"  name="end_datetime" placeholder="End Date" value="{{old('end_datetime')}}"/>
+                @if ($errors->has('end_datetime'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('end_datetime') }}</strong>
+                    </span>
+                @endif
+                <input class="form-control"  type="tel" name="phone" placeholder="Phone" value="{{old('phone')}}"/>
+                @if ($errors->has('phone'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('phone') }}</strong>
+                    </span>
+                @endif
+                <input class="form-control"  name="skills_needed" placeholder="Skills Needed" value="{{old('skills_needed')}}"/>
+                @if ($errors->has('skills_needed'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('skills_needed') }}</strong>
+                    </span>
+                @endif
+                <input class="form-control"  name="skills_taught" placeholder="Skills Taught" value="{{old('skills_taught')}}"/>
+                @if ($errors->has('skills_taught'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('skills_taught') }}</strong>
+                    </span>
+                @endif
+
+                <input class="form-control" type="number" name="fee" placeholder="Fee" value="{{old('fee')}}"/>
+                @if ($errors->has('fee'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('fee') }}</strong>
+                    </span>
+                @endif
+              <button type="submit" class="btn btn-primary"><i class="fa fa-university" aria-hidden="true"></i> Add Clinic</button>
+            </form>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="row">
+    <div class="col-md-8 col-md-offset-2">
+        <div class="panel panel-default">
+            <div class="panel-heading">Clinics</div>
+
+            <div class="panel-body">
+              <table class="table">
+                <thead>
+                  <tr>
+                    <th>Name</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  @foreach($clinics as $clinic)
+                  <tr>
+                    <td><a href="{{url('/coach/clinics/'.$clinic->id)}}" target="_blank">{{$clinic->name}}</a></td>
+                  </tr>
+                  @endforeach
+                </tbody>
+              </table>
+              {!! $clinics->links() !!}
+            </div>
+        </div>
+    </div>
+</div>
---
+++ resources/views/student/schools.blade.php
@@ -0,0 +1,42 @@
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Search School</div>
+
+                <div class="panel-body">
+                  <div class="table-responsive">
+                    @include('search.schools')
+                    <table class="table">
+                     <thead>
+                       <tr>
+                         <th>Name</th>
+                         <th>Website</th>
+                         <th>Min. GPA</th>
+                         <th>Min GPA Transfer</th>
+                         <th>In State Tuition</th>
+                         <th>Out State Tuition</th>
+                       </tr>
+                     </thead>
+                     <tbody>
+                       @foreach($schools as $school)
+                       <tr>
+                         <td><a href="{{url('/cheerleader/schools/'.$school->id)}}" target="_blank">{{$school->name}}</a></td>
+                         <td><a href="{{$school->website}}" target="_blank">{{$school->website}}</a></td>
+                         <td>{{$school->min_gpa}}</td>
+                         <td>{{$school->min_gpa_transfer}}</td>
+                         <td>${{money_format('%.2n', $school->in_state_tuition)}}</td>
+                         <td>${{money_format('%.2n', $school->out_state_tuition)}}</td>
+                       </tr>
+                       @endforeach
+
+                     </tbody>
+                   </table>
+
+                  {!! $schools->links() !!}
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
