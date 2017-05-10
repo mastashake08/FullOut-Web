057df746782f9de630505c3a2ddb8c2c14bb39af
Jyrone Parker <jyrone.parker@gmail.com>
2017-05-09 23:12:24 -0400

    dd

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -8,6 +8,7 @@ class NotificationController extends Controller
 {
     //
     public function webPush(Request $request){
+      dd($request->all());
       $user = $request->user;
       $user->updatePushSubscription($request->input('endpoint'), $request->input('keys.p256h'), $request->input('keys.auth'));
       $user->notify('GenericNotification');
