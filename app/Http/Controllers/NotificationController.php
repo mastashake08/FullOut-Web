482d304f7564c126fe29f1f82f1019c1382df631
Jyrone Parker <jyrone.parker@gmail.com>
2017-05-09 23:16:02 -0400

    debug

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -8,10 +8,10 @@ class NotificationController extends Controller
 {
     //
     public function webPush(Request $request){
-      var_dump($request->all());
-      exit();
+      //var_dump($request->all());
+      //exit();
       $user = $request->user;
-      $user->updatePushSubscription($request->input('endpoint'), $request->input('keys.p256h'), $request->input('keys.auth'));
+      $user->updatePushSubscription($request->input('endpoint'), $request->input('keys.p256dh'), $request->input('keys.auth'));
       $user->notify('GenericNotification');
       return response()->json([
         'success' => true
