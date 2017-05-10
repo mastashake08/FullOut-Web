7fe8783b76c802d510b5d07fe43eea76cb3cd9f4
Jyrone Parker <jyrone.parker@gmail.com>
2017-05-10 00:54:08 -0400

    sw

--- public/js/service-worker.js
+++ public/js/service-worker.js
@@ -1,8 +1,8 @@
 self.addEventListener('push', function(event) {
-  console.log(event.data.json());
+  console.log(event.data.json().message);
   if (event.data) {
     console.log('This push event has data: ', event.data.text());
-    var notification = new Notification(event.data.json().title);
+    var notification = new Notification(event.data.text().message);
   } else {
     console.log('This push event has no data.');
   }
