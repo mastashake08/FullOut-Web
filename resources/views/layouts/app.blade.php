615537c47c301e31b806b9de7843c4c2194299d5
Jyrone Parker <jyrone.parker@gmail.com>
2017-05-10 00:23:50 -0400

    scripts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <script src="/js/helpers.js"></script>
+
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
@@ -22,6 +22,7 @@
         localStorage.setItem("id",<?php echo auth()->user()->id ?>);
         @endif
     </script>
+    <script src="/js/helpers.js"></script>
 </head>
 <body>
     <div id="app">
