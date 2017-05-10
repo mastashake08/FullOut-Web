93f6206a72e5ff286a00944a62069721f78c87af
Jyrone Parker <jyrone.parker@gmail.com>
2017-05-10 00:48:44 -0400

    test

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -14,11 +14,22 @@ class NotificationController extends Controller
       $user = User::find($request->id);
       //var_dump($user);
       //exit();
-      
+
       $user->updatePushSubscription($request->input('subscription.endpoint'), $request->input('subscription.keys.p256dh'), $request->input('subscription.keys.auth'));
       $user->notify(new \App\Notifications\GenericNotification());
       return response()->json([
         'success' => true
       ]);
+    }
+
+    public function sendTest(){
+      //var_dump($request->all());
+      //exit();
+      $user = auth()->user();
+      //var_dump($user);
+      //exit();
+
+      $user->updatePushSubscription($request->input('subscription.endpoint'), $request->input('subscription.keys.p256dh'), $request->input('subscription.keys.auth'));
+      $user->notify(new \App\Notifications\GenericNotification());
     }
 }
