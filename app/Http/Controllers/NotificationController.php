c5e6e8831765bbe73a7e27df51133fbf7b37ea7d
Jyrone Parker <jyrone.parker@gmail.com>
2017-05-09 23:59:32 -0400

    auth

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -4,13 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\User;
+use Auth;
 class NotificationController extends Controller
 {
     //
     public function webPush(Request $request){
       //var_dump($request->all());
       //exit();
-      $user = auth()->user();
+      $user = Auth::user();
       $user->updatePushSubscription($request->input('endpoint'), $request->input('keys.p256dh'), $request->input('keys.auth'));
       $user->notify('GenericNotification');
       return response()->json([
