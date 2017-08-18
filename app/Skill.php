a51b83cc6117bfbe8e0f812fae3cab953b6ee067
Jyrone Parker <jyrone.parker@gmail.com>
2017-08-18 18:49:30 -0400

    casts

--- app/Skill.php
+++ app/Skill.php
@@ -10,6 +10,9 @@ class Skill extends Model
     protected $guarded = [];
     protected $casts = [
       'spring_floor_tumbling_skills' => 'array',
+      'hard_floor_tumbling_skills' => 'array',
+      'group_stunting_skills' => 'array',
+      'coed_stunting_skills' => 'array'
     ];
     public function user(){
       return $this->belongsTo('App\User');
