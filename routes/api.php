1e2a5bb8a45842b4d631379a551dfc2bf78e2616
Jyrone Parker <jyrone.parker@gmail.com>
2017-09-09 13:49:15 -0400

    api test

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,7 @@ use Illuminate\Http\Request;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
-});
+})->middleware('auth:api');
 
     Route::post('/register', 'ApiController@register');
 
